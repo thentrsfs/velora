71edc6eea3340fa651b470beee4af933064e1577
thentrsfs <filipstojkov54@gmail.com>
2026-05-25 20:47:36 +0200

    Add mobile responsiveness to Hero section by adjusting text sizes and container widths for smaller screens.

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -11,28 +11,28 @@ const Hero = () => {
 				muted
 				loop
 				playsInline
-				className='absolute inset-0 w-full object-cover '>
+				className='absolute inset-0 w-full h-full object-cover '>
 				<source
 					src='/videos/hero-video.mp4'
 					type='video/mp4'
 				/>
 			</video>
-			<div className='absolute inset-0 bg-bg/70' />
+			<div className='absolute inset-0 lg:bg-bg/70 bg-bg/75' />
 			<div className='absolute bottom-0 left-0 h-20 w-full bg-linear-to-b from-transparent to-bg' />
 			<div className='relative z-10 flex h-full items-center'>
 				<div className='mx-auto w-full max-w-7xl px-6'>
-					<div className='max-w-140'>
+					<div className='lg:max-w-150'>
 						<p className='mb-4 text-sm tracking-[0.3em] text-primary uppercase'>
 							Velora Café
 						</p>
 
-						<h1 className='font-clash-display text-7xl tracking-[0.02em] text-text'>
+						<h1 className='font-clash-display lg:text-7xl text-[40px] leading-none tracking-[0.02em] text-text'>
 							Crafted Moments,
 							<br />
 							Poured Daily.
 						</h1>
 
-						<p className='mt-6 max-w-md text-lg text-text-muted'>
+						<p className='mt-6 lg:max-w-md max-w-2xs text-lg text-text-muted'>
 							Experience specialty coffee and a warm, intentional atmosphere in
 							every cup.
 						</p>
