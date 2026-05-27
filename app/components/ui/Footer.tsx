671faa6029006b3c28fd05445dd4bc24b4f6e17c
thentrsfs <filipstojkov54@gmail.com>
2026-05-27 18:50:50 +0200

    Make footer responsive and add border top to it and increace gap between items in it.

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -1,44 +1,46 @@
 const Footer = () => {
 	return (
-		<footer className='absolute bottom-0 w-full flex items-center justify-between py-6 text-sm text-text-muted'>
-			<p className='text-sm text-text-muted'>
-				&copy; {new Date().getFullYear()} Velora. All rights reserved.
-			</p>
+		<footer className='absolute bottom-0 left-0 w-full text-sm text-text-muted border-t border-white/10'>
+			<div className='flex max-sm:flex-col max-sm:gap-8 items-center justify-between py-6 max-w-7xl mx-auto'>
+				<p className='text-sm text-text-muted'>
+					&copy; {new Date().getFullYear()} Velora. All rights reserved.
+				</p>
 
-			<div className='flex items-center gap-6 text-sm text-text-muted'>
-				<a
-					href='https://www.instagram.com/filip.webdev/'
-					target='_blank'
-					className='transition-colors duration-300 hover:text-primary'>
-					Instagram
-				</a>
+				<div className='flex items-center gap-6 text-sm text-text-muted'>
+					<a
+						href='https://www.instagram.com/filip.webdev/'
+						target='_blank'
+						className='transition-colors duration-300 hover:text-primary'>
+						Instagram
+					</a>
 
-				<a
-					href='https://www.linkedin.com/in/filip-stojkov-315773a1/'
-					target='_blank'
-					className='transition-colors duration-300 hover:text-primary'>
-					LinkedIn
-				</a>
+					<a
+						href='https://www.linkedin.com/in/filip-stojkov-315773a1/'
+						target='_blank'
+						className='transition-colors duration-300 hover:text-primary'>
+						LinkedIn
+					</a>
+
+					<a
+						href='https://www.filipstojkov.com'
+						target='_blank'
+						className='transition-colors duration-300 hover:text-primary'>
+						Portfolio
+					</a>
+				</div>
 
 				<a
 					href='https://www.filipstojkov.com'
 					target='_blank'
-					className='transition-colors duration-300 hover:text-primary'>
-					Portfolio
+					rel='noopener noreferrer'
+					className='text-sm group text-text-muted '>
+					Designed & Developed by
+					<span className='group-hover:text-primary transition-colors duration-300'>
+						{' '}
+						Filip Stojkov
+					</span>
 				</a>
 			</div>
-
-			<a
-				href='https://www.filipstojkov.com'
-				target='_blank'
-				rel='noopener noreferrer'
-				className='text-sm group text-text-muted '>
-				Designed & Developed by
-				<span className='group-hover:text-primary transition-colors duration-300'>
-					{' '}
-					Filip Stojkov
-				</span>
-			</a>
 		</footer>
 	);
 };
