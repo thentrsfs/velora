8ff50b07ab94216c04466d2a00747287cab4c11c
thentrsfs <filipstojkov54@gmail.com>
2026-05-28 17:25:52 +0200

    Add GSAP animations to Contact section

--- app/components/sections/Contact.tsx
+++ app/components/sections/Contact.tsx
@@ -1,26 +1,81 @@
+'use client';
+
+import { gsap } from 'gsap';
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import Form from '@/app/components/ui/Form';
 import Footer from '@/app/components/ui/Footer';
 
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 const Contact = () => {
+	const contactRef = useRef<HTMLDivElement>(null);
+
+	useGSAP(
+		() => {
+			const tl = gsap.timeline({
+				scrollTrigger: { trigger: contactRef.current, start: 'top center' },
+			});
+			tl.to('.contact-label', {
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				ease: 'power3.out',
+			})
+				.to(
+					'.contact-title',
+					{ opacity: 1, y: 0, duration: 1.2, ease: 'power4.out' },
+					'-=0.4',
+				)
+				.to(
+					'.contact-text',
+					{ opacity: 1, y: 0, duration: 1, ease: 'power4.out' },
+					'-=0.8',
+				)
+				.to(
+					'.contact-info',
+					{
+						opacity: 1,
+						y: 0,
+						duration: 1,
+						ease: 'power3.out',
+					},
+					'-=0.8',
+				)
+				.to(
+					'.form-container',
+					{
+						opacity: 1,
+						y: 0,
+						duration: 1,
+						ease: 'power3.out',
+					},
+					'-=0.8',
+				);
+		},
+		{ scope: contactRef },
+	);
 	return (
 		<section
 			id='contact'
+			ref={contactRef}
 			className='min-h-screen px-6 relative lg:py-32 pt-20 pb-60 flex items-center'>
 			<div className='grid lg:grid-cols-2 items-center max-md:gap-10 w-full lg:max-w-7xl mx-auto '>
 				<div className='lg:max-w-lg max-w-xs'>
-					<p className='mb-4 text-sm tracking-[0.3em] text-primary uppercase'>
+					<p className='mb-4 text-sm tracking-[0.3em] text-primary uppercase contact-label opacity-0 translate-y-10'>
 						Get in touch
 					</p>
 
-					<h2 className='font-display lg:text-6xl text-4xl leading-none text-text'>
+					<h2 className='font-display lg:text-6xl text-4xl leading-none text-text contact-title opacity-0 translate-y-10'>
 						Visit Velora
 					</h2>
 
-					<p className='mt-8 max-w-sm text-lg text-text-muted'>
+					<p className='mt-8 max-w-sm text-lg text-text-muted contact-text opacity-0 translate-y-10'>
 						Crafted coffee, warm atmosphere, and intentional moments — served
 						daily in the heart of the city.
 					</p>
-					<div className='mt-12 space-y-8 text-text-muted'>
+					<div className='mt-12 space-y-8 text-text-muted contact-info opacity-0 translate-y-10'>
 						<div>
 							<p className='text-sm uppercase tracking-[0.25em] text-white/40'>
 								Location
@@ -41,7 +96,7 @@ const Contact = () => {
 						</div>
 					</div>
 				</div>
-				<div>
+				<div className='form-container opacity-0 translate-y-10'>
 					<Form />
 				</div>
 			</div>
