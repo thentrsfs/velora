457465e39555cfbedbffadc31a82dc37a135563b
thentrsfs <filipstojkov54@gmail.com>
2026-05-28 15:32:41 +0200

    Change primary button to be anchor tag instead and add href to be a contact section.

--- app/components/ui/PrimaryBtn.tsx
+++ app/components/ui/PrimaryBtn.tsx
@@ -1,8 +1,10 @@
 const PrimaryBtn = () => {
 	return (
-		<button className='rounded-full border border-primary text-text py-2 px-5 text-sm lg:text-base hover:bg-primary hover:text-bg transition-all duration-300 cursor-pointer'>
+		<a
+			href='#contact'
+			className='rounded-full border border-primary text-text py-2 px-5 text-sm lg:text-base hover:bg-primary hover:text-bg transition-all duration-300 cursor-pointer'>
 			Reserve table
-		</button>
+		</a>
 	);
 };
 
