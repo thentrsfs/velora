eacefe5a6fc52de1a9fa591b9eb280b8b8fd1ba4
thentrsfs <filipstojkov54@gmail.com>
2026-05-30 01:06:08 +0200

    Change primary button to be a link

--- app/components/ui/PrimaryBtn.tsx
+++ app/components/ui/PrimaryBtn.tsx
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 const PrimaryBtn = () => {
 	return (
-		<a
-			href='#contact'
+		<Link
+			href='/#contact'
 			className='rounded-full border border-primary text-text py-2 px-5 text-sm lg:text-base hover:bg-primary hover:text-bg transition-all duration-300 cursor-pointer'>
 			Reserve table
-		</a>
+		</Link>
 	);
 };
 
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -62,11 +62,7 @@ const MenuPage = () => {
 							</p>
 
 							<div className='mt-10 flex gap-4'>
-								<Link
-									href='/#contact'
-									className='rounded-full border border-primary text-text py-2 px-5 text-sm lg:text-base hover:bg-primary hover:text-bg transition-all duration-300 cursor-pointer'>
-									Reserve table
-								</Link>
+								<PrimaryBtn />
 							</div>
 						</div>
 					</div>
