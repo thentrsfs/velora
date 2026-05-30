70f410790eaa2240b8673676d9a2b9294b31d25c
thentrsfs <filipstojkov54@gmail.com>
2026-05-30 13:32:46 +0200

    Make secondary button have a href prop

--- app/components/ui/SecondaryBtn.tsx
+++ app/components/ui/SecondaryBtn.tsx
@@ -1,10 +1,16 @@
 import Link from 'next/link';
 
-const SecondaryBtn = ({ children }: { children: React.ReactNode }) => {
+const SecondaryBtn = ({
+	children,
+	href,
+}: {
+	children: React.ReactNode;
+	href: string;
+}) => {
 	return (
 		<button>
 			<Link
-				href='/menu'
+				href={href}
 				className='group inline-flex items-center gap-2 text-[15px] tracking-widest text-text/80 transition-colors duration-300 hover:text-primary cursor-pointer'>
 				{children}
 				<span className='transition-transform duration-300 group-hover:translate-x-1'>
