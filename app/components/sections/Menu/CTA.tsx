e316fc3a90a082b0467ccaa347becb75c46e9824
thentrsfs <filipstojkov54@gmail.com>
2026-05-31 12:26:11 +0200

    Adjust menu items to be flex column and add gap to it, change CTA section to have primary button instead of secondary and remove button that was going over Link in secondary button

--- app/components/sections/Menu/CTA.tsx
+++ app/components/sections/Menu/CTA.tsx
@@ -1,18 +1,80 @@
+'use client';
+
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef } from 'react';
+
+import PrimaryBtn from '@/app/components/ui/PrimaryBtn';
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 const CTA = () => {
+	const ctaRef = useRef<HTMLDivElement>(null);
+
+	useGSAP(
+		() => {
+			const tl = gsap.timeline({
+				scrollTrigger: { trigger: ctaRef.current, start: 'top center' },
+			});
+
+			tl.to('.cta-label', {
+				opacity: 1,
+				y: 0,
+				duration: 0.8,
+				ease: 'power3.out',
+			})
+				.to(
+					'.cta-title',
+					{ opacity: 1, y: 0, duration: 1.2, ease: 'power4.out' },
+					'-=0.4',
+				)
+				.to(
+					'.cta-text',
+					{ opacity: 1, y: 0, duration: 1, ease: 'power4.out' },
+					'-=0.8',
+				);
+		},
+		{ scope: ctaRef },
+	);
 	return (
-		<section className='py-32 text-center'>
-			<h2 className='text-4xl lg:text-5xl font-display text-text'>
-				See You At Velora
-			</h2>
+		<section
+			ref={ctaRef}
+			className='lg:pb-34 lg:pt-20 flex items-center w-full border-t border-white/10'>
+			<div className='lg:max-w-7xl px-6 mx-auto w-full grid lg:grid-cols-2 items-center'>
+				<div className='lg:max-w-sm'>
+					<p className='mb-4 text-sm tracking-[0.3em] text-primary uppercase cta-label opacity-0 translate-y-10'>
+						Visit Velora
+					</p>
 
-			<p className='mt-5 text-text-muted max-w-sm mx-auto'>
-				Reserve your table and enjoy our handcrafted coffee in a warm,
-				intentional atmosphere.
-			</p>
+					<h1 className='font-clash-display lg:text-7xl text-[40px] leading-none tracking-[0.02em] text-text cta-title opacity-0 translate-y-10'>
+						See You At Velora
+					</h1>
+					<div className='cta-text opacity-0 translate-y-10'>
+						<p className='mt-6 lg:max-w-md max-w-2xs text-lg text-text-muted '>
+							Reserve your table and enjoy our handcrafted coffee in a warm,
+							intentional atmosphere.
+						</p>
 
-			<button className='mt-10 text-primary uppercase tracking-[0.25em] text-sm'>
-				Reserve Table →
-			</button>
+						<div className='mt-8 flex gap-4'>
+							<PrimaryBtn />
+						</div>
+					</div>
+				</div>
+
+				<div className='flex items-center justify-end'>
+					<div className='max-w-lg'>
+						<p className='font-clash-display text-4xl xl:text-[52px] leading-[1.05] text-text'>
+							&quot;Every cup <br /> tells a story.&quot;
+						</p>
+
+						<div className='mt-10 h-px w-20 bg-primary/30' />
+
+						<p className='mt-6 text-sm tracking-[0.3em] uppercase text-primary'>
+							Velora
+						</p>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
--- app/components/ui/SecondaryBtn.tsx
+++ app/components/ui/SecondaryBtn.tsx
@@ -8,16 +8,14 @@ const SecondaryBtn = ({
 	href: string;
 }) => {
 	return (
-		<button>
-			<Link
-				href={href}
-				className='group inline-flex items-center gap-2 text-[13px] tracking-[0.2em] uppercase text-text transition-colors duration-300 hover:text-primary cursor-pointer'>
-				{children}
-				<span className='transition-transform duration-300 group-hover:translate-x-1'>
-					→
-				</span>
-			</Link>
-		</button>
+		<Link
+			href={href}
+			className='group inline-flex items-center gap-2 text-[13px] tracking-[0.2em] uppercase text-text transition-colors duration-300 hover:text-primary cursor-pointer'>
+			{children}
+			<span className='transition-transform duration-300 group-hover:translate-x-1'>
+				→
+			</span>
+		</Link>
 	);
 };
 
