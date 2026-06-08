4098081a8da6770ceb9d733c1a37dc8c2ba25577
thentrsfs <filipstojkov54@gmail.com>
2026-06-08 20:02:14 +0200

    Fix the menu not changing when editing data in Sanity because of the cache

--- app/components/sections/Menu/Menu.tsx
+++ app/components/sections/Menu/Menu.tsx
@@ -5,7 +5,8 @@ import MenuContent from './MenuContent';
 export const revalidate = 0;
 
 const Menu = async () => {
-	const menuCategories = await client.fetch<MenuCategory[]>(`
+	const menuCategories = await client.fetch<MenuCategory[]>(
+		`
     *[_type == "category"]{
       _id,
       title,
@@ -16,7 +17,13 @@ const Menu = async () => {
         price
       }
     }
-  `);
+  `,
+		{},
+		{
+			cache: 'no-store',
+			next: { revalidate: 0 },
+		},
+	);
 	return <MenuContent menuCategories={menuCategories} />;
 };
 
